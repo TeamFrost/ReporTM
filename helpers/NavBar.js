6068ee511adac003bf32657f4bb45cfef183c13a
Simonesiss <simona.radulea@gmail.com>
2020-11-22 17:34:21 +0200

    Create NavBar

---
+++ helpers/NavBar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, StyleSheet, TouchableHighlight } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+import { DrawerActions } from '@react-navigation/native';
+
+export default function NavBar() {
+    const navigation = useNavigation();
+
+    return (
+        <View style={styles.bottomMenu}>
+            <TouchableHighlight
+                // onPress={() => console.log('PressedMenu!')}
+                onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+                style={{ flex: 1 }}>
+                <View style={styles.bottomMenuTouchable}>
+                    <Icon
+                        name='md-menu'
+                        size={30}
+                        color='#593480'
+                        style={{ marginRight: 25 }}
+                    />
+                </View>
+            </TouchableHighlight>
+
+            <TouchableHighlight
+                onPress={() => navigation.navigate('Home')}
+                style={{ flex: 1 }}>
+                <View style={styles.bottomMenuTouchable}>
+                    <Icon
+                        name='md-home'
+                        size={30}
+                        color='#593480'
+                        style={{ marginLeft: 25 }}
+                    />
+                </View>
+            </TouchableHighlight>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    bottomMenu: {
+        flexDirection: 'row',
+        width: '100%',
+        height: '8%',
+        borderTopColor: '#593480',
+        borderTopWidth: 0.5,
+    },
+    bottomMenuTouchable: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#fefefe"
+    }
+
+})
