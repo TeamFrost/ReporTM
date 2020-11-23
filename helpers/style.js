2964ce02ff46c1f46370fde1f55898bf62476600
Simonesiss <simona.radulea@gmail.com>
2020-11-23 21:21:32 +0200

    Create style file
    Contains colors and dimensions

---
+++ helpers/style.js
@@ -0,0 +1,20 @@
+import { Dimensions } from "react-native";
+
+export const screenHeight = Math.round(Dimensions.get('window').height);
+export const screenWidth = Math.round(Dimensions.get('window').width);
+
+export const colors = {
+    white: '#FFF',
+    black: '#000',
+    darkPurple: '#593480',
+    lightPurple: '#ECDAF27F',
+    pressedLightPurple: '#ECDAF2D0',
+    pressedWhite: '#FEFEFE',
+    purple: '#BB6BD9',
+    searchBarGray: '#E4E0E9',
+    shadowGray: '#CCC',
+    textGray: '#8F92A1',
+    textHelpGray: '#ADADAD',
+    textYellow: '#FFC61B',
+}
+
