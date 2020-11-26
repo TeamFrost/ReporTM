055d5c6d29945e3406390b281e3e837dc703ff46
Simonesiss <simona.radulea@gmail.com>
2020-11-26 21:37:55 +0200

    Export category array

---
+++ helpers/category.js
@@ -0,0 +1,26 @@
+export const category = [
+    {
+        value: 'Groapa',
+        title: 'Groapa',
+    },
+    {
+        value: 'Graffiti',
+        title: 'Graffiti',
+    },
+    {
+        value: 'Gunoi',
+        title: 'Gunoi',
+    },
+    {
+        value: 'Iluminat Stradal',
+        title: 'Iluminat Stradal',
+    },
+    {
+        value: 'Poluare',
+        title: 'Poluare',
+    },
+    {
+        value: 'Parcare',
+        title: 'Parcare',
+    },
+]
