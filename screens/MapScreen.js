863ed6db6153a36f1985c3aa5687d7fd9286c1bb
EdiOnetiu <edyonetiu@gmail.com>
2020-11-30 00:27:32 +0200

    Add Map Screen Callout & Tags

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from "expo-status-bar";
 import MapView, { Marker } from 'react-native-maps';
-import { Image, View, StyleSheet } from "react-native";
+import { Image, View, StyleSheet, ScrollView, TouchableOpacity, Text } from "react-native";
 import { SearchBar } from 'react-native-elements';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import NavBar from '../helpers/navbar'
 import { colors, screenHeight, screenWidth } from "../helpers/style";
-import { watchReportsData } from '../redux/actions/reports'
-
+import { watchReportsData } from '../redux/actions/reports';
+import { category } from '../helpers/category';
+import { Callout } from 'react-native-maps';
+import { color } from 'react-native-reanimated';
 
 const mapStateToProps = (state) => {
     return {
@@ -51,8 +54,36 @@ function MapScreen({ ...props }) {
                             coordinate={marker.coordinates}
                             title={marker.category}
                             description={marker.description}
-                            pinColor={colors.purple}
-                        />
+                            pinColor={
+                                (marker.category === "groapa") ? colors.groapa :
+                                    (marker.category === "graffiti") ? colors.graffiti :
+                                        (marker.category === "gunoi") ? "aqua" :
+                                            (marker.category === "iluminat") ? colors.iluminat :
+                                                (marker.category === "poluare") ? colors.poluare :
+                                                    (marker.category === "parcare") ? colors.parcare : colors.black
+                            }
+                        >
+                            <Callout tooltip>
+                                <View>
+                                    <View style={styles.calloutView}>
+                                        <Text style={{ height: 180, position: 'relative', bottom: 40, right: 17, top: -70, width: 200 }}>
+                                            <Image
+                                                imageResizeMode='cover'
+                                                source={require('../assets/Test.png')}
+                                            />
+                                        </Text>
+                                        <View style={{ marginTop: -40 }}>
+                                            <Text style={{ fontSize: 16 }}>
+                                                {marker.description}
+                                            </Text>
+                                        </View>
+
+                                    </View>
+                                </View>
+
+                            </Callout>
+
+                        </Marker>
                     ))}
                 </MapView>
 
@@ -69,6 +100,26 @@ function MapScreen({ ...props }) {
                     onChangeText={(text) => setSearch(text)}
                     inputStyle={{ marginRight: 22 }}
                 />
+                <ScrollView
+                    horizontal
+                    scrollEventThrottle={1}
+                    showsHorizontalScrollIndicator={false}
+                    height={50}
+                    style={styles.scrollTags}
+                    contentContainerStyle={styles.scrollContainter}
+                >
+                    {category.map((category, index) => (
+                        <TouchableOpacity key={index} style={styles.tagButton}>
+                            <Icon
+                                type="font-awesome"
+                                name={category.icon}
+                                size={16}
+                                style={{ marginRight: 5, paddingTop: 2, color: category.color }}
+                            />
+                            <Text style={styles.tagText}>{category.title}</Text>
+                        </TouchableOpacity>
+                    ))}
+                </ScrollView>
                 <StatusBar style="auto" />
             </KeyboardAwareScrollView>
             <NavBar />
@@ -111,6 +162,40 @@ const styles = StyleSheet.create({
     searchBarInput: {
         backgroundColor: colors.white,
         height: "100%",
+
+    },
+    scrollTags: {
+        position: "absolute",
+        top: 130,
+    },
+    tagText: {
+        fontSize: 14
+    },
+    tagButton: {
+        flexDirection: "row",
+        backgroundColor: colors.white,
+        borderRadius: 20,
+        padding: 5,
+        paddingHorizontal: 10,
+        height: 35,
+        marginRight: 10,
+        borderWidth: 1,
+        borderColor: colors.searchBarGray,
+        elevation: 3
+    },
+    scrollContainter: {
+        paddingLeft: "10%",
+        paddingRight: "10%",
+
+    },
+    calloutView: {
+        flexDirection: 'column',
+        width: 200,
+        alignSelf: 'flex-start',
+        backgroundColor: colors.white,
+        borderRadius: 10,
+        borderColor: "#ccc",
+        padding: 15,
 
     },
 })
