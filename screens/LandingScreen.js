8a320e3b25b01f7d7f60fba304be286cc9e59f3c
Simonesiss <simona.radulea@gmail.com>
2020-12-03 18:36:17 +0200

    Update Landing

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
