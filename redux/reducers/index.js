bd67d0fb8b0050cbff6fb5ba870827a78fab1a72
Simonesiss <simona.radulea@gmail.com>
2020-12-09 22:02:21 +0200

    Add combined store

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import reports from './reports';
 import auth from './auth';
 
-export default rootReducer = () => combineReducers({
+export default rootReducer = combineReducers({
     reports,
-    auth
+    auth,
 })
--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
-import reports from './reducers/reports'
 import rootReducers from './reducers/index'
 
 export const store = createStore(rootReducers, applyMiddleware(thunkMiddleware));
