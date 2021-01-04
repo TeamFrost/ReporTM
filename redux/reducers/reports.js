6a3fd4553faf486c0addef452f5fb3d64666ba73
Simonesiss <simona.radulea@gmail.com>
2021-01-05 00:07:47 +0200

    Update reports reducer

--- redux/reducers/reports.js
+++ redux/reducers/reports.js
@@ -1,16 +1,40 @@
-import * as types from '../actions/auth/actionTypes'
+import { TextPropTypes } from 'react-native';
+import * as types from '../actions/reports/actionTypes'
 
 const initialState = {
+    doneFetching: false,
+    isFetching: false,
+    hasError: false,
+    errorMessage: '',
     reportsData: [],
 };
 
 export default reports = (state = initialState, action) => {
     switch (action.type) {
-        case "setReportsData": {
+        case types.REQUEST_START: {
+            return {
+                ...initialState,
+                isFetching: true
+            };
+        }
+        case types.REQUEST_SUCCESS: {
             const { reportsData } = action;
             return {
                 ...state,
+                doneFetching: true,
+                isFetching: false,
                 reportsData
+            };
+        }
+        case types.REQUEST_ERROR: {
+            const { error } = action;
+            return {
+                ...state,
+                doneFetching: true,
+                isFetching: false,
+                hasError: true,
+                reportsData: [],
+                errorMessage: error
             };
         }
         default:
