cdfb239f07043a4f9f1bce7af25aca263d20e083
Simonesiss <simona.radulea@gmail.com>
2021-01-05 00:07:20 +0200

    Update firebase query request

--- redux/actions/reports/reports.js
+++ redux/actions/reports/reports.js
@@ -1,42 +1,56 @@
 import { firebase } from '../../../config/firebaseConfig';
+import * as types from './actionTypes';
 
-export const watchReportsData = () => {
-    return function (dispatch) {
-        const reportsRef = firebase.firestore().collectionGroup('sub_reports');
-        reportsRef
-            .onSnapshot(
-                querySnapshot => {
-                    let reportsData = []
-                    querySnapshot.forEach(doc => {
-                        const report = doc.data()
-                        report.id = doc.id
-                        const parent = doc.ref.parent.parent.id
-                        const authorId = report.author
-                        firebase.firestore().collection("users").where("id", "==", authorId)
-                            .get()
-                            .then(function (querySnapshot) {
-                                querySnapshot.forEach(function (doc) {
-                                    // doc.data() is never undefined for query doc snapshots
-                                    const author = doc.data().username;
-                                    const reportFinal = { ...report, parent, author };
-                                    reportsData.push(reportFinal)
-                                });
-                            })
-                            .catch(function (error) {
-                                console.log("Error getting documents: ", error);
-                            });
+export const watchReportsData = () => dispatch => {
+    dispatch(requestStart());
+    const reportsRef = firebase.firestore().collectionGroup('sub_reports');
+    reportsRef
+        .onSnapshot(querySnapshot => {
+            let reportsData = []
+            querySnapshot.forEach(async doc => {
+                const report = doc.data()
+                report.id = doc.id
+                const parent = doc.ref.parent.parent.id
+                const authorId = report.author
+                await firebase.firestore().collection("users")
+                    .doc(authorId)
+                    .get()
+                    .then(firestoreDocument => {
+                        if (!firestoreDocument.exists) {
+                            dispatch(requestError("Document does not exist!"))
+                        }
+                        const username = firestoreDocument.data().username;
+                        const avatar = firestoreDocument.data().avatar;
+                        const userInfo = { username, avatar }
+                        const reportFinal = { ...report, parent, userInfo };
+                        reportsData.push(reportFinal)
+                    })
+                    .catch(error => {
+                        dispatch(requestError(error));
                     });
-                    const actionSetReportsData = setReportsData(reportsData)
-                    dispatch(actionSetReportsData);
-                },
-                error => {
-                    console.log(error)
-                }
-            )
-    }
-};
 
-const setReportsData = (reportsData) => ({
-    type: "setReportsData",
+            })
+
+            console.log(reportsData)
+            dispatch(requestSuccess(reportsData))
+
+        }),
+        (error) => {
+            dispatch(requestError(error))
+        }
+}
+
+const requestStart = () => ({
+    type: types.REQUEST_START
+});
+
+const requestSuccess = reportsData => ({
+    type: types.REQUEST_SUCCESS,
     reportsData
 });
+
+const requestError = error => ({
+    type: types.REQUEST_ERROR,
+    error
+});
+
