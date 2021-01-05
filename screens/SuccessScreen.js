16923d5db7b799e5e2152470f839b7b0849c5c83
EdiOnetiu <edyonetiu@gmail.com>
2021-01-06 00:03:01 +0200

    Add SuccessScreen

--- screens/SuccessScreen.js
+++ screens/SuccessScreen.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import { StatusBar } from "expo-status-bar";
-import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
-import Icon from 'react-native-vector-icons/Ionicons';
+import { Text, View, StyleSheet, TouchableHighlight, Image } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 
 import NavBar from '../helpers/navbar'
 import { colors, screenHeight } from "../helpers/style";
 
-export default function SuccessScreen() {
+export default function SuccessScreen({ ...props }) {
 
     const onButtonPress = () => {
-        navigation.navigate('Home')
+        props.navigation.navigate('Home')
     }
 
     return (
         <View style={styles.container}>
-            <Text>Success</Text>
-            <TouchableHighlight underlayColor='#593480' onPress={onButtonPress} style={styles.button}>
-                <View style={{ flexDirection: "row", alignItems: "center" }}>
-                    <Text style={styles.buttonText}>Pagina Principala</Text>
-                    <Icon active name='md-arrow-forward' style={styles.arrowIcon} />
-                </View>
-            </TouchableHighlight>
-            <NavBar />
+            <View style={styles.textView}>
+                <Image
+                    source={require("../assets/Success.png")}
+                    style={styles.icon}
+                />
+                <Image
+                    source={require("../assets/Ellipse_1_s.png")}
+                    style={styles.ellipse1}
+                />
+                <Image
+                    source={require("../assets/Ellipse_2_s.png")}
+                    style={styles.ellipse2}
+                />
+            </View>
+            <View style={{ ...styles.textView, height: screenHeight / 3, width: '80%', justifyContent: 'space-around', }}>
+                <Text style={styles.text}>Formular trimis!</Text>
+                <Text style={{ ...styles.text, fontSize: 14 }}>Sesizarea ta a fost inregistrata cu succes. Intoarce-te la pagina principala.</Text>
+                <TouchableHighlight
+                    style={styles.confirmButton}
+                    onPress={onButtonPress}
+                    underlayColor='#E9DBF8'
+                >
+                    <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }} colors={['#C17BDB', '#9853C5', '#6C4397']} style={{ ...styles.confirmButton, width: '100%' }}>
+                        <View style={{ alignItems: "center" }}>
+                            <Text style={styles.buttonText}>Pagina principală</Text>
+                        </View>
+                    </LinearGradient>
+                </TouchableHighlight>
+            </View>
             <StatusBar style="auto" />
         </View>
     );
@@ -32,7 +53,48 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.white,
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "flex-start",
     },
+    confirmButton: {
+        height: 45,
+        borderRadius: 18,
+        justifyContent: 'center',
+        width: '100%',
+        alignSelf: "center",
+        marginTop: 5,
+        elevation: 5,
+
+    },
+    buttonText: {
+        fontSize: 18,
+        color: colors.white,
+        fontWeight: "bold",
+    },
+    ellipse1: {
+        position: "absolute",
+        top: "20%",
+        right: "0%",
+        width: 59,
+        height: 124,
+    },
+    ellipse2: {
+        position: "absolute",
+        top: "60%",
+        left: "-1%",
+        width: 47,
+        height: 103,
+    },
+    textView: {
+        height: screenHeight / 1.75,
+        width: '100%',
+        alignItems: "center",
+        justifyContent: 'center',
+    },
+    text: {
+        fontWeight: "bold",
+        fontSize: 30,
+        textAlign: "center"
+    }
+
 
 })
