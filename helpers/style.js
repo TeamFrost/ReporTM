82c7f3bb907a2b5231a2c5856c20d5d93efe3734
Simonesiss <simona.radulea@gmail.com>
2021-01-16 17:04:05 +0200

    Add themes

--- helpers/style.js
+++ helpers/style.js
@@ -28,3 +28,64 @@ export const colors = {
     poluare: "#83b1cb",
     parcare: "#9c280e",
 }
+
+const themeLight = {
+    white: '#FFF',
+    black: '#000',
+    backgroundColor: "#FFFFFF",
+    navColor: "#FEFEFE",
+    homeCardsColor: "#ECDAF27F",
+
+
+    darkPurple: '#593480',
+    lightPurple: '#ECDAF27F',
+    pressedLightPurple: '#ECDAF2D0',
+    pressedWhite: '#FEFEFE',
+    purple: '#BB6BD9',
+    purple2: '#B269CD',
+    red: "#D35933",
+    searchBarGray: '#E4E0E9',
+    shadowGray: '#CCC',
+    textGray: '#8F92A1',
+    textHelpGray: '#ADADAD',
+    textYellow: '#FFC61B',
+    linkBlue: '#0000F0',
+
+
+    groapa: "#d37e53",
+    graffiti: "#593480",
+    gunoi: "#C0EAFF",
+    iluminat: "#FFCE3C",
+    poluare: "#83b1cb",
+    parcare: "#9c280e",
+}
+
+const themeDark = {
+    white: '#FFF',
+    black: '#000',
+    backgroundColor: "242424",
+    navColor: "#121212",
+    homeCardsColor: "#505050",
+
+    darkPurple: '#593480',
+    lightPurple: '#ECDAF27F',
+    pressedLightPurple: '#ECDAF2D0',
+    pressedWhite: '#FEFEFE',
+    purple: '#BB6BD9',
+    purple2: '#B269CD',
+    red: "#D35933",
+    searchBarGray: '#E4E0E9',
+    shadowGray: '#CCC',
+    textGray: '#8F92A1',
+    textHelpGray: '#ADADAD',
+    textYellow: '#FFC61B',
+    linkBlue: '#0000F0',
+
+
+    groapa: "#d37e53",
+    graffiti: "#593480",
+    gunoi: "#C0EAFF",
+    iluminat: "#FFCE3C",
+    poluare: "#83b1cb",
+    parcare: "#9c280e",
+}
