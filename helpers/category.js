1e3c05f5726b6ca5da2229044cec4f0b1b16faf1
Simonesiss <simona.radulea@gmail.com>
2021-01-18 21:12:13 +0200

    Update colors

--- helpers/category.js
+++ helpers/category.js
@@ -1,40 +1,38 @@
-import { colors } from "./style";
-
 export const category = [
     {
         value: 'groapa',
         title: 'Groapă',
         icon: "exclamation-triangle",
-        color: colors.groapa,
+        color: "#d37e53",
     },
     {
         value: 'graffiti',
         title: 'Graffiti',
         icon: "spray-can",
-        color: colors.graffiti,
+        color: "#593480",
     },
     {
         value: 'gunoi',
         title: 'Gunoi',
         icon: "trash",
-        color: colors.gunoi,
+        color: "#C0EAFF",
     },
     {
         value: 'iluminat',
         title: 'Iluminat Stradal',
         icon: "lightbulb",
-        color: colors.iluminat,
+        color: "#FFCE3C",
     },
     {
         value: 'poluare',
         title: 'Poluare',
         icon: "smog",
-        color: colors.poluare,
+        color: "#83b1cb",
     },
     {
         value: 'parcare',
         title: 'Parcare',
         icon: "parking",
-        color: colors.parcare,
+        color: "#9c280e",
     },
 ]
