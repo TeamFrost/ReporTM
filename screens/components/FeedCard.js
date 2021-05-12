1994327024645742d709e050b8704a88ef69ad48
EdiOnetiu <edyonetiu@gmail.com>
2021-05-12 20:30:29 +0300

    Add photo modal

--- screens/components/FeedCard.js
+++ screens/components/FeedCard.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity, TouchableHighlight, Modal } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { Divider } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { Avatar } from 'react-native-paper';
 import { connect } from 'react-redux';
 
-import { screenWidth, themeColors } from "../../helpers/style";
+import { screenWidth, screenHeight, themeColors } from "../../helpers/style";
 import { firebase } from "../../config/firebaseConfig";
 import { restoreSession } from '../../redux/actions/auth/auth';
 
@@ -22,6 +23,11 @@ function FeedCard({ userName, userAvatar, adress, time, photo, description, upvo
 
     const [styles, setStyles] = useState(styleSheetFactory(themeColors.themeLight))
     const [colors, setColors] = useState(themeColors.themeLight)
+    const [pickerVisibility, setPickerVisibility] = useState(false)
+
+    const togglePicker = () => {
+        setPickerVisibility(!pickerVisibility)
+    }
 
     useEffect(() => {
         if (theme) {
@@ -67,12 +73,12 @@ function FeedCard({ userName, userAvatar, adress, time, photo, description, upvo
         if (currentUser) {
             if (currentUser.upvotedreports.includes(id)) {
                 return (
-                    <Icon name='check-circle' size={25} color={colors.upvotePressed} style={{ paddingLeft: 15, paddingRight: 5 }} solid />
+                    <Icon name='check-circle' size={25} color={colors.upvotePressed} style={styles.upvoteIcon} solid />
                 )
             }
             else {
                 return (
-                    <Icon name='arrow-alt-circle-up' size={25} color={colors.darkPurple} style={{ paddingLeft: 15, paddingRight: 5 }} />
+                    <Icon name='arrow-alt-circle-up' size={25} color={colors.darkPurple} style={styles.upvoteIcon} />
                 )
             }
         }
@@ -84,12 +90,12 @@ function FeedCard({ userName, userAvatar, adress, time, photo, description, upvo
                 <View style={styles.cardHeaderLeft}>
                     <Avatar.Image size={50} source={userAvatar} />
                     <View style={styles.cardHeaderText}>
-                        <Text style={{ fontSize: 18, fontWeight: 'bold', color: colors.textColor }}>{userName}</Text>
-                        <View style={{ flexDirection: 'row', alignItems: "center" }}>
+                        <Text style={styles.usernameText}>{userName}</Text>
+                        <View style={styles.cardFooterLeft}>
                             <Icon name='map-marker-alt' type="font-awesome-5" size={10} style={{ paddingRight: 7, color: colors.textColor }} />
                             <Text style={{ fontSize: 12, color: colors.textColor }}>{adress}</Text>
                         </View>
-                        <View style={{ flexDirection: 'row', alignItems: "center" }}>
+                        <View style={styles.cardFooterLeft}>
                             <Icon name='clock' type="font-awesome-5" size={10} style={{ paddingRight: 5, marginLeft: -1, color: colors.textGray }} />
                             <Text style={{ fontSize: 12, color: colors.textGray }}>{time}</Text>
                         </View>
@@ -101,23 +107,27 @@ function FeedCard({ userName, userAvatar, adress, time, photo, description, upvo
                     </TouchableOpacity>
                 </View>
             </View>
+
             <View style={styles.cardContent}>
-                <Image
-                    source={photo}
-                    style={{ width: 160, height: 160 }}
-                />
+                <TouchableOpacity onPress={() => togglePicker()}>
+                    <Image
+                        source={photo}
+                        style={{ width: 160, height: 160 }}
+                    />
+                </TouchableOpacity>
                 <Text numberOfLines={10} style={styles.cardDescription}>
                     {description}
                 </Text>
             </View>
-            <Divider style={{ height: 1, backgroundColor: colors.textGray, width: '90%', alignSelf: "center" }} />
+
+            <Divider style={styles.divider} />
+
             <View style={styles.cardFooter}>
                 <View style={styles.cardFooterLeft}>
                     <TouchableOpacity onPress={onUpvotePress}>
                         {displayIcon()}
                     </TouchableOpacity>
-                    <Text style={{ color: colors.textColor }}>{upvotes}</Text>
-                    <Text style={{ color: colors.textColor }}> Aprobări</Text>
+                    <Text style={styles.normalText}>{upvotes} {upvotes === 1 ? <Text>Aprobare</Text> : <Text>Aprobări</Text>}</Text>
                 </View>
                 <View style={styles.rightBottomTag}>
                     <TouchableOpacity style={styles.tagButton2} >
@@ -127,18 +137,33 @@ function FeedCard({ userName, userAvatar, adress, time, photo, description, upvo
                             size={15}
                             style={{ marginRight: 5, paddingTop: 1, color: color }}
                         />
-                        <Text style={{ color: colors.textColor }}>{tag}</Text>
+                        <Text style={styles.normalText}>{tag}</Text>
                     </TouchableOpacity>
                 </View>
             </View>
+
+            <Modal visible={pickerVisibility} animationType={"fade"} transparent={true}>
+                <View style={styles.modalCover}>
+                    <View style={styles.modal}>
+                        <Image
+                            source={photo}
+                            style={styles.modalImageStyle}
+                        />
+                        <TouchableHighlight onPress={() => togglePicker()} style={styles.exitModal}>
+                            <View style={styles.modalIconClose}>
+                                <Icon name='times' size={25} color={colors.darkPurple} />
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                    <StatusBar style="auto" backgroundColor='#0000007F' />
+                </View>
+            </Modal>
         </View>
     );
 }
 
 const styleSheetFactory = (colors) => StyleSheet.create({
     card: {
-        // alignItems: 'center',
-        // justifyContent: 'center',
         alignSelf: 'center',
         borderRadius: 20,
         margin: 10,
@@ -158,7 +183,7 @@ const styleSheetFactory = (colors) => StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
         height: 70,
-        width: '100%'
+        width: '100%',
     },
     cardHeaderLeft: {
         flexDirection: "row",
@@ -216,6 +241,57 @@ const styleSheetFactory = (colors) => StyleSheet.create({
     rightBottomTag: {
         width: 110,
         alignItems: "center"
+    },
+    modalCover: {
+        width: screenWidth,
+        height: screenHeight,
+        backgroundColor: '#0000007F',
+    },
+    modal: {
+        position: 'absolute',
+        top: 122,
+        width: "85%",
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: "center",
+        backgroundColor: colors.modalColor,
+        elevation: 5
+    },
+    usernameText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: colors.textColor
+    },
+    upvoteIcon: {
+        paddingLeft: 15,
+        paddingRight: 5
+    },
+    normalText: {
+        color: colors.textColor
+    },
+    divider: {
+        height: 1,
+        width: '90%',
+        backgroundColor: colors.textGray,
+        alignSelf: "center"
+    },
+    modalImageStyle: {
+        width: screenWidth / 10 * 8.5,
+        height: screenWidth / 10 * 8.5
+    },
+    exitModal: {
+        position: 'absolute',
+        top: '5%',
+        right: '5%',
+    },
+    modalIconClose: {
+        width: 35,
+        height: 35,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: colors.backgroundColor,
+        borderRadius: 5,
+        elevation: 5
     }
 })
 
