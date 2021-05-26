17662861313cba3e02c379ec4d81be7bb14416c8
Simonesiss <simona.radulea@gmail.com>
2021-05-26 23:55:45 +0300

    Update index.js

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import reports from './reports';
 import auth from './auth';
 import theme from './colorTheme'
+import translations from './translations'
 
 export default rootReducer = combineReducers({
     reports,
     auth,
-    theme
+    theme,
+    translations,
 })
