d6fa9e9e0ab8f6ee5ea1da3b515aeba445058722
SimonaRadulea <48724811+SimonaRadulea@users.noreply.github.com>
2021-06-29 17:40:59 +0300

    Update firebaseConfig.js

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,13 +3,13 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAz_odPz6hIMfhlFRbLXfeLETbsP-6WKZI',
-    authDomain: 'reportm-40f3e.firebaseapp.com',
-    databaseURL: 'https://reportm-40f3e.firebaseio.com',
-    projectId: 'reportm-40f3e',
-    storageBucket: 'reportm-40f3e.appspot.com',
-    messagingSenderId: '72027984488e',
-    appId: '1:72027984488:android:584bb7d5a01b0ec8bc61dd',
+    apiKey: '***',
+    authDomain: '***',
+    databaseURL: '***',
+    projectId: '***',
+    storageBucket: '***',
+    messagingSenderId: '***',
+    appId: '***',
 };
 
 if (!firebase.apps.length) {
